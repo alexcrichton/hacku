293cf34a04d228bf0dbc1a6613e59725f264bf5a
Alex Crichton <alex@alexcrichton.com>
2010-09-21 10:04:46 -0400

    Initial rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackU::Application.config.secret_token = '825a93ed48a132a37a2beea3c93b0b530f95190aec50683d0b21d20537e6065ad018ee3025eadc34271a47dfb660e02a6bb0696bb8b99498d916c170b5024ab4'
