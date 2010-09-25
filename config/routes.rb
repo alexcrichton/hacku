4695ef0abd3d2dbdad5f3ada6217b1c479b30a87
Alex Crichton <alex@alexcrichton.com>
2010-09-25 11:16:54 -0400

    Remove old

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ HackU::Application.routes.draw do
   get 'login' => 'sessions#create'
   get 'login/new' => 'sessions#new', :as => 'new_login'
 
-  get ':action' => 'static', :action => /sample|graph/
+  get ':action' => 'static', :action => /graph/
   put 'similarity(.:format)' => 'info#similarity'
 
   get 'graph' => 'static#graph'
