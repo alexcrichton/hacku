fe5fc1739ea98070b1bcc5ec5253d30369d99849
Alex Crichton <alex@alexcrichton.com>
2010-09-25 11:40:27 -0400

    it works!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-  def fb_login_url
-    @login_url = MiniFB.oauth_url(
-        Rails.application.config.fb_app_id, login_url,
-        :scope => 'user_interests,user_likes,friends_likes,friends_interests')
-  end
 end
